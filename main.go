9b12dedc74421dec0b75dee9aef364375578e1b6
Prasaddhulgande <prasaddhulgande11@gmail.com>
2026-03-09 10:18:44 +0530

    Added PUT UPDATE APIs

--- main.go
+++ main.go
@@ -4,20 +4,108 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
-
+var r= gin.Default()
 func main() {
 
-	r:= gin.Default()
-	r.GET("/ping", func(c *gin.Context){
-		c.JSON(http.StatusOK, gin.H{
-			"Message": "Pong",
-			"New Message": "Server running on port 8000",
-		})
-	})
+
+	POSTAPI(r)
+	GETIdNewId(r)
+	GETAPI(r)
+	PUTAPI(r)
+	UpdateAPI(r)
+
 	r.Run(":8000")
  
 	
 
+}
+func GETAPI(router *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"Message": "Pong running on port :8000",
+			"Message 2": "This is GET API",
+		})
+	})
+
+}
+func GETIdNewId(router *gin.Engine) {
+	r.GET("/me/:id/:newId", func(c *gin.Context){
+		id:= c.Param("id")
+		newId:= c.Param("newId")
+
+		c.JSON(http.StatusOK, gin.H{
+			"User ID": id,
+			"New User ID":newId,
+		})
+	})
+	
+}
+func POSTAPI(router *gin.Engine) {
+	//r:= gin.Default() // r declare globally 
+	r.POST("/me", func(c *gin.Context){
+		// Add Authontications with email and pass
+		type MeRequest struct {
+			Email string `json:"email" binding: "required"`
+			Password string `json: "password"`
+		}
+		//If you binding email as a required then need to handle error
+		var meRequest MeRequest
+		if err:= c.BindJSON(&meRequest); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"email": meRequest.Email,
+			"password": meRequest.Password,
+		})
+	})
+	//r.Run(":8000") // router provided as a parameter into this func
+}
+// PUT API
+
+func PUTAPI(router *gin.Engine) {
+	r.PUT("/me", func(c *gin.Context){
+
+		type MeRequest struct {
+			Email string `json: "email" binding:"request"`
+			Password string `json: "password"`
+		}
+		var meRequest MeRequest
+
+		if err:= c.BindJSON(&meRequest); err!= nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return 
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"email": meRequest.Email,
+			"password": meRequest.Password,
+		})
+	})
+}
+
+func UpdateAPI(router *gin.Engine){
+	r.PATCH("/me", func(c *gin.Context){
+      type MeRequest struct{
+		Email string `json: "email" binding:"required"`
+		Password string `json: "password"`
+	  }
+	  var meRequest MeRequest
+
+	  if err:= c.BindJSON(&meRequest); err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return 
+	  }
+	  c.JSON(http.StatusOK, gin.H{
+		"email": meRequest.Email,
+		"password": meRequest.Password,
+	  })
+	})
 }
 
 // func RunServer(){
